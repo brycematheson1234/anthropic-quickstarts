5f45c222a3e5744a37be41e5c36e28d9b79778fd
bryce <bryce@canva.com>
2025-05-24 17:38:59 +1000

    clone repo functionality

--- computer-use-demo/setup.sh
+++ computer-use-demo/setup.sh
@@ -19,3 +19,37 @@ source .venv/bin/activate
 pip install --upgrade pip
 pip install -r dev-requirements.txt
 pre-commit install
+
+# Git repository setup for Docker
+echo ""
+echo "Setting up git repository cache for Docker..."
+
+# Create directories for Docker volume mounting
+mkdir -p ./git-cache ./workspace ./host-files
+
+# Set up git repository cache
+# Usage: ./setup.sh [REPO_URL] [BRANCH] [REPO_NAME]
+REPO_URL="${1:-${CANVA_REPO_URL:-org-2562356@github.com:Canva/canva.git}}"
+BRANCH="${2:-${CANVA_BRANCH:-master}}"
+REPO_NAME="${3:-canva}"
+CACHE_PATH="./git-cache/$REPO_NAME"
+
+echo "Using repository: $REPO_URL"
+echo "Using branch: $BRANCH"
+echo "Using repo name: $REPO_NAME"
+
+if [[ -d "$CACHE_PATH/.git" ]]; then
+    echo "Found existing git cache, updating..."
+    cd "$CACHE_PATH"
+    git fetch origin "$BRANCH" --depth=1
+    git reset --hard "origin/$BRANCH"
+    cd ../..
+else
+    echo "Performing initial shallow clone..."
+    git clone --depth=1 --branch="$BRANCH" --single-branch "$REPO_URL" "$CACHE_PATH"
+fi
+echo "Git cache ready at $CACHE_PATH"
+
+echo ""
+echo "Setup complete! You can now run:"
+echo "  docker-compose up"
